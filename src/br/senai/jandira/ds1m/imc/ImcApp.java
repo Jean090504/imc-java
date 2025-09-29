7710e1ed075fa653d97587f9452e7b93c6685bdd
JEAN COSTA ALVES DA SILVA <25203640@CFP127ALUNO.LOCAL>
2025-09-29 08:13:57 -0300

    Entrada de dados

--- src/br/senai/jandira/ds1m/imc/ImcApp.java
+++ src/br/senai/jandira/ds1m/imc/ImcApp.java
@@ -1,7 +1,14 @@
 package br.senai.jandira.ds1m.imc;
 
+import br.senai.jandira.ds1m.imc.model.CalculadoraImc;
+
 public class ImcApp {
     public static void main(String[] args) {
         System.out.println("Iniciando programa...");
+
+        CalculadoraImc imc = new CalculadoraImc();
+
+        imc.obterDados();
+
     }
 }
--- src/br/senai/jandira/ds1m/imc/model/CalculadoraImc.java
+++ src/br/senai/jandira/ds1m/imc/model/CalculadoraImc.java
@@ -1,4 +1,17 @@
 package br.senai.jandira.ds1m.imc.model;
+import java.util.Scanner;
 
 public class CalculadoraImc {
+    String nome;
+    double peso;
+    double altura;
+
+    public void obterDados(){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        nome = leitor.nextLine();
+
+
+    }
 }
